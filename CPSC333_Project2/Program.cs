59441174fe99c120e50047e9f836e94f23319e71
Torbear <torykepler@csu.fullerton.edu>
2014-11-05 19:01:10 -0800

    final

--- CPSC333_Project2/Program.cs
+++ CPSC333_Project2/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Project Name: AES CBC with Cipher Text Stealing
+// Class: CPSC333
+// Names: Tory Kepler, Jon Clay
+// Purpose: Implement CBC using AES encryption
+// Module Info: Performs AES CBC encryption
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
@@ -8,86 +14,61 @@ using System.Security.Cryptography;
 
 namespace CPSC333_Project2
 {
-    class Program
+    class encryption
     {
         static void Main(string[] args)
         {            
             string plaintext = File.ReadAllText("plaintext.txt");
-            string ciphertext = File.ReadAllText("ciphertext.txt");
 
+            // Remove all whitespace in plaintext prior to encryption
             plaintext = plaintext.Replace(" ", "");
             
+            // Create Aes object
             Aes myAes = Aes.Create();
+
+            // Set Mode to CBC, KeySize to 128, Blocksize to 128
             myAes.Mode = CipherMode.CBC;
             myAes.KeySize = 128;
             myAes.BlockSize = 128;
+
+            // Initialize IV and Key
             byte[] IVbytearray = new byte[] {0x85,0x71,0xD1,0xAD,0xA2,0x4F,0xEC,0x59,0xDB,0xA6,0xEB,0xF8,0x19,0x43,0x7A,0x34};
             byte[] Keybytearray = new byte[] {0x54, 0x46, 0xF6, 0x09, 0xBF, 0xB0, 0x4D, 0xBD, 0xFF, 0xFC, 0x11, 0xC1, 0xE5, 0x36, 0x3E, 0xEA};
+
+            // Set IV and Key on myAes
             myAes.IV = IVbytearray;
             myAes.Key = Keybytearray;
 
-            string IV = "";
-            string Key = "";
-
+            // Get byte array for the plaintext
             byte[] inBlock = Encoding.ASCII.GetBytes(plaintext);
 
-            int i = 0;
-
-            //Console.WriteLine(inBlock.Length);
-
-            foreach (byte b in inBlock)
+            // Pad the last block with 0x00
+            while (inBlock.Length % 16 != 0)
             {
-                Console.WriteLine(b);
+                Array.Resize(ref inBlock, inBlock.Length + 1);
+                inBlock[inBlock.Length - 1] = 0x00;
             }
 
-            while (i < inBlock.Length)
-            {
+            // Call encrypt function, returns encrypted byte string
+            byte[] encryptedString = EncryptString(myAes, inBlock);
 
-                i += 128;
-            }
-
-            IV = BitConverter.ToString(myAes.IV);
-            Key = BitConverter.ToString(myAes.Key);
-
-            Console.WriteLine("IV (Hex) : " + IV);
-            Console.WriteLine("Key (Hex) : " + Key + "\n");
-
-            byte[] encryptedString = EncryptString(myAes, plaintext);
-            string decryptedString = DecryptString(myAes, encryptedString);
-
-            string decrypted = DecryptString(myAes, StringToByteArray(ciphertext));
-
+            // Print encrypted bytes
             Console.WriteLine("Encrypted Bytes: " + BitConverter.ToString(encryptedString) + "\n");
 
+            // Write encrypted bytes to text file
             File.WriteAllText("ciphertext.txt", BitConverter.ToString(encryptedString).Replace("-",""));
 
-            Console.WriteLine("The String was: " + decrypted);
         }
 
-        public static byte[] EncryptString (Aes AesObj, string plaintext)
+        public static byte[] EncryptString(Aes AesObj, byte[] inBlock)
         {
-            byte[] inBlock = Encoding.ASCII.GetBytes(plaintext);
+            // Creates encryptor based on AesObj's settings (mode, key size, IV size, etc.)
             ICryptoTransform xfrm = AesObj.CreateEncryptor();
+
+            // Get encrypted data
             byte[] outblock = xfrm.TransformFinalBlock(inBlock, 0, inBlock.Length);
 
             return outblock;
-        }
-
-        public static string DecryptString(Aes AesObj, byte[] encryptedText)
-        {
-            ICryptoTransform xfrm = AesObj.CreateDecryptor();
-            byte[] outBlock = xfrm.TransformFinalBlock(encryptedText, 0, encryptedText.Length);
-
-            return Encoding.ASCII.GetString(outBlock);
-        }
-
-        public static byte[] StringToByteArray(String hex)
-        {
-            int NumberChars = hex.Length;
-            byte[] bytes = new byte[NumberChars / 2];
-            for (int i = 0; i < NumberChars; i += 2)
-                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
-            return bytes;
         }
     }
 }
---
+++ CPSC333_Project2/decryption.cs
@@ -0,0 +1,67 @@
+﻿// Project Name: AES CBC with Cipher Text Stealing
+// Class: CPSC333
+// Names: Tory Kepler, Jon Clay
+// Purpose: Implement CBC using AES encryption & decryption
+// Module Info: Performs AES CBC decryption
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CPSC333_Project2
+{
+    class decryption
+    {
+        static void decrypt()
+        {
+            // Read in ciphertext
+            string ciphertext = File.ReadAllText("ciphertext.txt");
+
+            // Create AES object
+            Aes myAes = Aes.Create();
+            
+            // Set AES parameters
+            myAes.Mode = CipherMode.CBC;
+            myAes.KeySize = 128;
+            myAes.BlockSize = 128;
+
+            // Initialize IV and Key
+            byte[] IVbytearray = new byte[] {0x85,0x71,0xD1,0xAD,0xA2,0x4F,0xEC,0x59,0xDB,0xA6,0xEB,0xF8,0x19,0x43,0x7A,0x34};
+            byte[] Keybytearray = new byte[] {0x54, 0x46, 0xF6, 0x09, 0xBF, 0xB0, 0x4D, 0xBD, 0xFF, 0xFC, 0x11, 0xC1, 0xE5, 0x36, 0x3E, 0xEA};
+            
+            // Set IV and Key
+            myAes.IV = IVbytearray;
+            myAes.Key = Keybytearray;
+
+            // Call decrypt function
+            string decrypted = DecryptString(myAes, StringToByteArray(ciphertext));
+
+            // Print decrypted string
+            Console.WriteLine("The String was: " + decrypted);
+        }
+
+        public static string DecryptString(Aes AesObj, byte[] encryptedText)
+        {
+            // Create decryptor object
+            ICryptoTransform xfrm = AesObj.CreateDecryptor();
+            
+            // Get decrypted data
+            byte[] outBlock = xfrm.TransformFinalBlock(encryptedText, 0, encryptedText.Length);
+
+            return Encoding.ASCII.GetString(outBlock);
+        }
+
+        public static byte[] StringToByteArray(String hex)
+        {
+            int NumberChars = hex.Length;
+            byte[] bytes = new byte[NumberChars / 2];
+            for (int i = 0; i < NumberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+    }
+}
